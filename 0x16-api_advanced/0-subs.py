1e5273f2f9c9f2b1af939ced3b1cfe94233659ec
daliamohamed2000 <dm1956@fayoum.edu.eg>
2024-06-22 00:52:20 -0700

    a

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
     import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
